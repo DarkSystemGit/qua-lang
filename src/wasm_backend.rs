b2ae245537469fad7cf4db48eda8b05a89b63837
Gavin Morrow <gavinfmorrow@gmail.com>
2025-03-29 17:31:45 -0400

    Add skeleton for gen_wasm

--- src/wasm_backend.rs
+++ src/wasm_backend.rs
@@ -1,1 +1,12 @@
+use crate::ast::Program;
 
+pub fn gen_wasm(program: Program) -> WasmBinary {
+    todo!()
+}
+
+pub struct WasmBinary {}
+impl WasmBinary {
+    pub fn into_bytes(self) -> Vec<u8> {
+        todo!()
+    }
+}
