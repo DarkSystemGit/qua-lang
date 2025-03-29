c0c7bd35344e6291a7a5da2e2b9b9cabb830c1bb
Gavin Morrow <gavinfmorrow@gmail.com>
2025-03-29 19:16:03 -0400

    Module w/ type section

--- src/wasm_backend.rs
+++ src/wasm_backend.rs
@@ -1,55 +1,85 @@
-use crate::ast::Program;
+use crate::ast::{self, Program};
 
-pub fn gen_wasm(program: Program) -> Module {
-    let mut module = Module::new();
+pub fn gen_wasm(program: Program) -> Vec<u8> {
+    let mut module = wasm::Module::default();
 
-    todo!()
-}
+    for stmt in program {
+        match stmt {
+            ast::Stmt::Let(binding) => match binding.metadata {
+                ast::BindingMetadata::Var => todo!(),
+                ast::BindingMetadata::Func {
+                    arguments,
+                    upvalues,
+                } => {
+                    // Everything is boxed (ie an I32)
+                    let param_tys = arguments.iter().map(|_| wasm::ValType::I32).collect();
+                    let result_ty = wasm::ValType::I32;
+                    let ty = wasm::FuncType {
+                        params: param_tys,
+                        // Functions can only have 1 return type as of now
+                        results: vec![result_ty],
+                    };
 
-pub struct Module {
-    function_types: Vec<FuncType>,
-    functions: Vec<Func>,
-}
-
-impl Module {
-    pub fn new() -> Self {
-        Module {
-            function_types: Vec::new(),
-            functions: Vec::new(),
+                    let ty_idx = module.ty_sec.insert(ty);
+                }
+            },
+            ast::Stmt::Expr(expr) => todo!(),
         }
     }
 
-    pub fn into_bytes(self) -> Vec<u8> {
-        todo!()
+    module.into_bytes()
+}
+
+mod wasm {
+    use std::collections::HashMap;
+
+    #[derive(Default)]
+    pub struct Module {
+        pub ty_sec: TypeSection,
     }
-}
 
-struct FuncTypeIdx(usize);
-struct FuncType {
-    parameters: Vec<ValueType>,
-    results: Vec<ValueType>,
-}
+    impl Module {
+        pub fn into_bytes(self) -> Vec<u8> {
+            todo!()
+        }
+    }
 
-enum ValueType {
-    Num(NumType),
-}
+    #[derive(Default)]
+    pub struct TypeSection {
+        types_map: HashMap<FuncType, TypeIdx>,
+        types: Vec<FuncType>,
+    }
 
-enum NumType {
-    I32,
-    I64,
-    F32,
-    F64,
-}
+    impl TypeSection {
+        pub fn insert(&mut self, ty: FuncType) -> TypeIdx {
+            if let Some(idx) = self.types_map.get(&ty) {
+                *idx
+            } else {
+                let next_idx = self.types.len();
+                self.types.push(ty.clone());
 
-struct Func {
-    ty: FuncTypeIdx,
-    locals: Vec<ValueType>,
-    body: Vec<Instr>,
-}
+                let idx = TypeIdx(next_idx);
+                self.types_map.insert(ty, idx);
 
-enum Instr {
-    I32Const(i32),
-    I64Const(i64),
-    F32Const(f32),
-    F64Const(f64),
+                idx
+            }
+        }
+    }
+
+    #[derive(Clone, Copy, Debug)]
+    pub struct TypeIdx(usize);
+
+    #[derive(Clone, Debug, PartialEq, Eq, Hash)]
+    pub struct FuncType {
+        pub params: Vec<ValType>,
+        pub results: Vec<ValType>,
+    }
+
+    #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+    pub enum ValType {
+        I32,
+        I64,
+        F32,
+        F64,
+    }
 }
